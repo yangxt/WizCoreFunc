675ceb5eccaaab24b91a920122d0bb0fcdd84eb7
dzpqzb <yishuiliunian@gmail.com>
2012-09-26 09:22:15 +0800

    Initial Commit

---
+++ WizCoreFunc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WizCoreFunc
+//
+//  Created by wiz on 12-9-26.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WizCoreFunc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WizCoreFunc
+//
+//  Created by wiz on 12-9-26.
+//  Copyright (c) 2012年 cn.wiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
